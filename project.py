ea88a600dca53cfc60ea0777705c19f76b159f20
lokopopopo <63489540+lokopopopo@users.noreply.github.com>
2024-02-13 10:38:53 +0000

    Create project.py
    Just the file with the class

---
+++ project.py
@@ -0,0 +1,15 @@
+class Graph:
+    def __init__(self, name, age, location):
+        self.name = name
+        self.age = age
+        self.location = location
+    def __str__():
+        pass
+    def add_member(self):
+        pass
+    def add_relationship(self):
+        pass
+    def find_friends(self):
+        pass
+    def shortest_path(self):
+        pass
